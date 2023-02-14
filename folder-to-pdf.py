7ac12bbc3bfc807e8067833cb6b9ec3d3f68e7f2
sbarrientos2 <sebastianbarrientosa@gmail.com>
2023-02-13 22:30:39 -0500

    folder-to-pdf now lets the user know if the file has been created or if it has been updated if it existed

--- folder-to-pdf.py
+++ folder-to-pdf.py
@@ -1,5 +1,6 @@
 import io
 import os
+from datetime import datetime
 from PIL import Image
 from PyPDF4 import PdfFileMerger
 
@@ -12,8 +13,24 @@ image_filenames = sorted([f for f in os.listdir(image_folder) if f.endswith('.jp
 # Extract the last component of the image_folder path as the output filename
 output_filename = os.path.basename(image_folder) + '.pdf'
 
+# Create the full output file path using os.path.join()
+output_file_path = os.path.join(image_folder, output_filename)
+
+# Initialize the backup_file_path variable to None
+backup_file_path = None
+
+# Check if the output file already exists
+if os.path.exists(output_file_path):
+    # Rename the existing file with a timestamp
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    backup_file_path = os.path.join(image_folder, f'{output_filename}.{timestamp}')
+    os.rename(output_file_path, backup_file_path)
+
+    # Print a message to the console to let the user know the file was updated
+    print(f'{output_filename} file updated')
+
 # Create a PDF file and open it in write binary mode in the same directory as the input images
-pdf_file = open(os.path.join(image_folder, output_filename), 'wb')
+pdf_file = open(output_file_path, 'wb')
 
 # Create a PyPDF4 PdfFileMerger object
 pdf_merger = PdfFileMerger()
@@ -36,3 +53,9 @@ pdf_merger.write(pdf_file)
 
 # Close the PDF file
 pdf_file.close()
+
+# Print a message to the console to let the user know the file was created or updated
+if backup_file_path is not None:
+    print(f'{output_filename} file updated')
+else:
+    print(f'{output_filename} file created')
